6e0dc3efa6a8452f38f13cac2e977b9070374ce2
atharvanaphade <naphade21@gmail.com>
2021-06-26 08:43:24 +0530

    created an app for running code

--- judge_server/settings.py
+++ judge_server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=a4e!s4a#8zk!6i26@te&a^b%2af-yv8yqxibu6md_v_k4!69_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'runner',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
